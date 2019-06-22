b3a9adac85824921b6e9c6ca41cff29e35412ccc
cvssea <42877793+cvssea@users.noreply.github.com>
2019-06-22 23:35:03 +0300

    remove default port

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 
 morgan.token('body', req => {
@@ -94,7 +96,7 @@ app.delete('/api/persons/:id', (req, res) => {
   res.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
